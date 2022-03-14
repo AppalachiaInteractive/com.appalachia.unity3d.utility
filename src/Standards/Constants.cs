25d8321c0af77774b319940447db26dffffc2b4c
Chris Schubert <chris.a.schubert25@gmail.com>
2022-03-13 20:57:24 -0400

    Refactoring into Control/ComponentGroup/Component Hierarchy underneath widgets.

--- src/Standards/Constants.cs
+++ src/Standards/Constants.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine;
 
 namespace Appalachia.Utility.Standards
 {
@@ -7,6 +8,16 @@ namespace Appalachia.Utility.Standards
         #region Constants and Static Readonly
 
         public static readonly DateTime UNIX_EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public const int REFERENCE_RESOLUTION_HEIGHT = 1080;
+        public const int REFERENCE_RESOLUTION_WIDTH = 1920;
+
+        public static readonly Vector2 REFERENCE_RESOLUTION = new Vector2(
+            REFERENCE_RESOLUTION_WIDTH,
+            REFERENCE_RESOLUTION_HEIGHT
+        );
+
+        public static readonly Vector2 SCREEN_CENTER = REFERENCE_RESOLUTION * .5F;
 
         #endregion
     }
